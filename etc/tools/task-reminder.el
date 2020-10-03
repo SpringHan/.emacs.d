572c11ac38096eeadeb0df830ac0b344c785ee00
SpringHan <3379184879@qq.com>
2020-10-03 23:13:18 +0800

    Modified task-reminder and snippets

--- etc/tools/task-reminder.el
+++ etc/tools/task-reminder.el
@@ -35,15 +35,26 @@
 										 (read-string "Enter the deadline time(HH:mm): ")
 										 (read-string "Enter the task content: "))))
 
+(defun task-reminder-get-tasks ()
+	"The function to get the tasks from task file.")
+
+(defun task-reminder-get-task-status (task)
+	"Get the TASK's status and return it.
+When it's done, return t.
+Otherwise return nil.")
+
 (defun task-reminder-process ()
 	"Start the process."
-	(while task-reminder-status))
+	(when (null task-reminder-status)
+		(cancel-timer task-reminder-timer)
+		(setq task-reminder-timer nil)))
 
 (defun task-reminder-start ()
 	"The task-reminder main program."
 	(interactive)
 	(if (not (null task-reminder-status))
 			(message "[Spring Emacs]: Task-Reminder has already running!")
-		(task-reminder-process)))
+		(setq task-reminder-timer
+					(run-at-time t 5 'task-reminder-process))))
 
 (provide 'task-reminder)
