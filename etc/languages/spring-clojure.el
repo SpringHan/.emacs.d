5b752087fedea266c35dc95992b60bb32ab1e495
SpringHan <3379184879@qq.com>
2021-02-23 09:52:49 +0800

    feat(spring-clojure): new function about run code

--- etc/languages/spring-clojure.el
+++ etc/languages/spring-clojure.el
@@ -1,7 +1,32 @@
 ;;; This is the clojure file.
 
+(defun spring/cider-eval-last-sexp-in-repl ()
+  "Eval last sexp in repl."
+  (interactive)
+  (let ((sexp (substring-no-properties (cider-last-sexp)))
+        buffer)
+    (with-current-buffer (current-buffer)
+      (when (derived-mode-p 'clojure-mode)
+        (setq sexp (concat (substring sexp 0 1)
+                           (cider-current-ns)
+                           "/"
+                           (substring sexp 1)))))
+    (catch 'stop
+      (dolist (buf (buffer-list))
+        (when (string-match-p "^\\*cider-repl \\(.*\\)\\*" (buffer-name buf))
+          (setq buffer buf)
+          (throw 'stop t))))
+    (when buffer
+      (with-current-buffer buffer
+        (end-of-buffer)
+        (insert sexp)
+        (cider-repl-return))
+      (while (not (eq (current-buffer) buffer))
+        (other-window 1)))))
+
 (gpack clojure-mode
-  :var (clojure-toplevel-inside-comment-form . t))
+  :var (clojure-toplevel-inside-comment-form . t)
+  :key (clojure-mode-map . ("C-c M-l" . spring/cider-eval-last-sexp-in-repl)))
 
 (gpack cider
   :hook (clojure-mode-hook . cider-mode))
