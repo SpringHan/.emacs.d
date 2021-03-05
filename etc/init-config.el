5d11f975e2f7b3a2a6d5d149809dfe46b0542870
SpringHan <3379184879@qq.com>
2021-03-05 20:35:38 +0800

    modifiy(init-config): Private config

--- etc/init-config.el
+++ etc/init-config.el
@@ -23,5 +23,9 @@
 (require 'task-reminder)
 (require 'run-code)
 (require 'init-macros)
+;;; Private settings
+(when (file-exists-p (locate-user-emacs-file "private.el"))
+  (load-file (locate-user-emacs-file "private.el"))
+  (require 'private))
 
 (provide 'init-config)
