55f0a1d192e4d6c78e47ce891696424764d3ab00
SpringHan <3379184879@qq.com>
2021-08-10 23:59:18 +0800

    modify(init-company): modified the settings about completion

--- etc/settings/init-complete.el
+++ etc/settings/init-complete.el
@@ -5,7 +5,11 @@
 (gpack company-c-headers)
 
 (gpack company
-  :hook (after-init-hook . global-company-mode)
+  :hook ((after-init-hook . global-company-mode)
+         (emacs-lisp-mode-hook . (lambda ()
+                                   (setq-local company-backends
+                                               (append '(company-elisp)
+                                                       company-backends)))))
   :var ((company-show-numbers . t)
         (company-idle-delay . 0)
         (company-echo-delay . 0))
@@ -19,20 +23,17 @@
               (define-key company-active-map (kbd "C-n") #'company-select-next)
               (define-key company-active-map (kbd "C-p") #'company-select-previous)
               (setq company-backends
-                    '((company-dabbrev company-keywords company-files company-capf)))
-              (add-hook 'emacs-lisp-mode-hook #'(lambda ()
-                                                  (setq-local company-backends
-                                                              (append '(company-elisp) company-backends))))))
+                    '((company-dabbrev-code company-dabbrev company-keywords company-files company-capf)))))
   :key ("C-' i" . company-yasnippet))
 
 (gpack company-box
   :hook (company-mode-hook . company-box-mode))
 
 (gpack company-web
-  :config (progn
-            (add-to-list 'company-backends 'company-web-html)
-            (add-to-list 'company-backends 'company-web-jade)
-            (add-to-list 'company-backends 'company-web-slim)))
+  :hook (web-mode-hook . (lambda ()
+                           (setq-local company-backends
+                                       (append '(company-web-html company-web-jade company-web-slim)
+                                               company-backends)))))
 
 (gpack company-tabnine
   :hook ((python-mode-hook go-mode-hook c-mode-hook c++-mode-hook) . (lambda ()
