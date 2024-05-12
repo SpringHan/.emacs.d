94075c2f75fce65845990eb2e7518e52381af61b
springhan <3379184879@qq.com>
2024-05-13 00:14:25 +0800

    Added ELisp treesit parser

--- etc/settings/init-treesit.el
+++ etc/settings/init-treesit.el
@@ -9,7 +9,8 @@
         (cpp . ("https://github.com/tree-sitter/tree-sitter-cpp"))
         (c . ("https://github.com/tree-sitter/tree-sitter-c"))
         (dart . ("https://github.com/UserNobody14/tree-sitter-dart"))
-        (python . ("https://github.com/tree-sitter/tree-sitter-python")))
+        (python . ("https://github.com/tree-sitter/tree-sitter-python"))
+        (elisp . ("https://github.com/Wilfred/tree-sitter-elisp")))
       major-mode-remap-alist
       (append major-mode-remap-alist
               '((js-json-mode . json-ts-mode))))
@@ -21,5 +22,6 @@
       (treesit-install-language-grammar (car lang)))))
 
 (add-hook 'emacs-startup-hook #'spring/initialize-treesit-languages)
+(add-hook 'emacs-lisp-mode-hook (lambda () (treesit-parser-create 'elisp)))
 
 (provide 'init-treesit)
